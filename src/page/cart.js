f78b7837f15e9474db177b82a20189dacf6b0deb
rezkirahman <rezkirahman1996@gmail.com>
2022-12-05 08:21:12 +0700

    deploy

--- src/page/cart.js
+++ src/page/cart.js
@@ -100,7 +100,7 @@ export default function Cart() {
         //change this to the script source you want to load, for example this is snap.js sandbox env
         const midtransScriptUrl = "https://app.sandbox.midtrans.com/snap/snap.js";
         //change this according to your client-key
-        const myMidtransClientKey = process.env.REACT_APP_MIDTRANS_CLIENT_KEY;
+        const myMidtransClientKey = SB-Mid-client-c2bc8ynN7yxUd99M;
     
         let scriptTag = document.createElement("script");
         scriptTag.src = midtransScriptUrl;
